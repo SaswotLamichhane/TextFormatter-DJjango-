dd3c79a92690738761a5cfd036afb9fd4598ff07
S.lamichhane <58847742+slamichhnetune@users.noreply.github.com>
2020-03-28 07:03:39 +0545

    Delete urls.py

--- testsas/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.Home),
-    path('cap', views.cap, name = 'caption'),
-    path('analyze', views.analyze, name = 'analyze')
-]
